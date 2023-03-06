3f645585c6a6bdeabd28656b1fe650fc4cea0bc1
Nguyen Tai Duc <taiduc8098@gmail.com>
2023-03-06 15:29:31 +0700

    Baithuchanh0703

--- BTH2002/Controllers/StudentController.cs
+++ BTH2002/Controllers/StudentController.cs
@@ -1,14 +1,22 @@
 using Microsoft.AspNetCore.Mvc;
 namespace BTH2002.Controllers;
-{
+
   public class StudentController : Controller
     {
-       public IActionResult Index()
+      public IActionResult Index()
+      {
+        return View();
+      }
+      [HttpPost]
+       public IActionResult Index( string name)
     {
+      string strReturn = " Hello, " + name;
+      // gui du lieu ve view
+      ViewBag.abc = strReturn;
         return View();
     }
 }  
-}
+
     
 
 
