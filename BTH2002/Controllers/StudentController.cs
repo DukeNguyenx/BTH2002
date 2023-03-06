f2e2ef431b6bd817627bbdba4c76bddfb4cbf13a
Nguyen Tai Duc <taiduc8098@gmail.com>
2023-03-06 16:19:36 +0700

    first commit

--- BTH2002/Controllers/StudentController.cs
+++ BTH2002/Controllers/StudentController.cs
@@ -15,6 +15,23 @@ namespace BTH2002.Controllers;
       ViewBag.abc = strReturn;
         return View();
     }
+     public IActionResult Tong()
+      {
+        return View();
+      }
+    [HttpPost]
+      public IActionResult Tong(string Number)
+      {
+        int so = Convert.ToInt32(Number);
+        int tong = 0;
+        while (so > 0)
+        {
+          tong = tong + so%10;
+          so = so/10;
+        }
+        ViewBag.Sum = "Tong cac chu so cua so" + Number + " = " + tong;  
+        return View();
+      }
 }  
 
     
